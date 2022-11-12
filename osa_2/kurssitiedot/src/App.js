f089ee57115f6b8635280b8a17785c8de8cc9cfc
bobriska <61400981+bobriska@users.noreply.github.com>
2022-11-12 20:40:57 +0200

    Tehtävät 2.1.-2.5.

--- osa_2/kurssitiedot/src/App.js
+++ osa_2/kurssitiedot/src/App.js
@@ -1,86 +1,55 @@
-import React from 'react'
-import Course from './components/Course'
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
+const Content = (props) => {
+  return (
+    <>
+      <Part part_name={props.parts[0].name} part_exercises={props.parts[0].exercises} />
+      <Part part_name={props.parts[1].name} part_exercises={props.parts[1].exercises} />
+      <Part part_name={props.parts[2].name} part_exercises={props.parts[2].exercises} />
+    </>
+  )
+}
 
+const Part = (props) => {
+  return(
+  <p>
+    {props.part_name} {props.part_exercises}
+  </p>
+  )
+}
+
+const Total = (props) => {   
+  return (
+    <p>Number of exercises {props.exercises.map(value => value.exercises).reduce((a, b) => a + b, 0)}</p>
+  )
+}
 
 const App = () => {
-  const courses = 
-    [
-      {
-        name: 'Half Stack application development',
-        id: 1,
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Hooks in action',
-            exercises: 16,
-            id: 4
-          }
-        ]
-      },
-      {
-        name: 'Java application development',
-        id: 2,
-        parts: [
-          {
-            name: 'Fundamentals of Java',
-            exercises: 2,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 3,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 4,
-            id: 3
-          },
-          {
-            name: 'Functions in action',
-            exercises: 16,
-            id: 4
-          }
-        ]
-      },
-      {
-        name: 'Node.js',
-        id: 3,
-        parts: [
-          {
-            name: 'Routing',
-            exercises: 3,
-            id: 1
-          },
-          {
-            name: 'Middlewares',
-            exercises: 7,
-            id: 2
-          }
-        ]
-      }
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ]
-  
 
   return (
     <div>
-      <h1>Webb development curriculum</h1>
-      {courses.map(kurssi => <Course key={kurssi.id} course={kurssi}/> )}
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total exercises={parts} />
     </div>
   )
 }
