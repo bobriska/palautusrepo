c4cb70471a69b6f09cb650a06681883323d5939b
bobriska <61400981+bobriska@users.noreply.github.com>
2022-11-12 20:34:17 +0200

    Add files via upload

--- osa_1/unicafe/src/App.js
+++ osa_1/unicafe/src/App.js
@@ -1,110 +1,56 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-const feedback_text = 'Anna palautetta'
-const statistics_text = 'Statistiikka'
-const good_text = 'Hyvä'
-const neutral_text = 'Neutraali'
-const bad_text = 'Huono'
-const all_text = 'Kaikki'
-const avg_text = 'Keskiarvo'
-const positives_text = 'Positiivisia'
-const no_feedback_text = 'Ei annettua palautetta'
-
-const StatisticLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td> 
-      <td>{props.value} {props.unit} </td>
-    </tr> 
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-}
-
-const Statistics = (props) => {
-  if(props.all_counter > 0) {
+const Statistics = ({good, neutral, bad}) => {
+  const sumall = good + neutral + bad
+  if (sumall === 0) return(
+        <p>No feedback given</p>
+      )
+  else {
+    const average = (good*1 + neutral*0 + bad*(-1))/sumall
     return(
-      <tbody>
-        <StatisticLine text={good_text} value={props.good_counter}/>
-        <StatisticLine text={neutral_text} value={props.neutral_counter}/>
-        <StatisticLine text={bad_text} value={props.bad_counter}/>
-        <StatisticLine text={all_text} value={props.all_counter}/>
-        {/* two following default to 0 if NaN */}
-        <StatisticLine text={avg_text} value={(props.all_sum / props.all_counter) || 0}/>
-        <StatisticLine text={positives_text} unit='%' value={(props.good_counter / props.all_counter * 100) || 0}/>
-      </tbody>
-    )
-  } else {
-    return(
-      <tbody><tr>
-        <td>{no_feedback_text}</td>
-      </tr></tbody>
-    )
+      <table>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={sumall}/>
+        <StatisticLine text="average" value={average}/>
+        <StatisticLine text="positive" value={good / sumall}/>
+      </table>
+    ) 
   }
 }
+
+const StatisticLine = ({text, value}) => {
+  return(
+    <tr><td>{text}</td> <td>{value}</td></tr>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const [ good_counter, set_good ] = useState(0)
-  const [ neutral_counter, set_neutral ] = useState(0)  
-  const [ bad_counter, set_bad ] = useState(0)
-  const [ all_sum, set_sum ] = useState(0)
-  const [ all_counter, set_all ] = useState(0)
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handle_good = () => {
-    set_good(good_counter + 1)
-    set_all(all_counter + 1)
-    set_sum(all_sum + 1)
-  }
-
-  const handle_neutral = () => {
-    set_neutral(neutral_counter + 1)
-    set_all(all_counter + 1)
-    set_sum(all_sum + 0)
-  }
-
-  const handle_bad = () => {
-    set_bad(bad_counter + 1)
-    set_all(all_counter + 1)
-    set_sum(all_sum - 1)
-  }
-
-
+  const handleGood = () => setGood(good + 1)
+  const handeNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <h1>{feedback_text}</h1>
- 
-      <p>
-        <Button
-          handleClick={handle_good}
-          text={good_text}
-        />
-        <Button
-          handleClick={handle_neutral}
-          text={neutral_text}
-        />     
-        <Button
-          handleClick={handle_bad}
-          text={bad_text}
-        />          
-      </p>
-      
-      <h1> {statistics_text} </h1>
-      <table>
-        <Statistics good_counter={good_counter}
-                    neutral_counter={neutral_counter}
-                    bad_counter={bad_counter}
-                    all_counter={all_counter}
-                    all_sum={all_sum}/>
-      </table>
+      <h1> give feedback </h1>
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handeNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-
   )
 }
 
